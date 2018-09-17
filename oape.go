2463331b8e9dbdaa00b2e4ad5365c82f66c73884
encima <encima@gmail.com>
2018-09-17 15:28:01 +0200

    config updated

--- oape.go
+++ oape.go
@@ -5,10 +5,6 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
 	o := openape.NewServer("/home/encima/dev/go/c3s-oape/config")
-=======
-	o := openape.NewServer("$HOME/dev/go/c3s-oape/config")
->>>>>>> ae91e297b7fd9f6aca703d46144be28f42dd1afb
 	o.RunServer()
 }
